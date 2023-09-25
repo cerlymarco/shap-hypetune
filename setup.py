df52667240574624333230780c6b49d917509369
Santiago SILVA <16252054+sssilvar@users.noreply.github.com>
2023-09-25 12:17:29 +0200

    Minor update release number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.2.6'
+VERSION = '0.2.7'
 PACKAGE_NAME = 'shap-hypetune'
 AUTHOR = 'Marco Cerliani'
 AUTHOR_EMAIL = 'cerlymarco@gmail.com'
